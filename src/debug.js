72cc9027525eb46cf2cf062c4a9d5e8dc6cac9ae
VeshonaS <aniahshell1@gmail.com>
2024-04-23 14:30:15 -0400

    did short res

--- src/debug.js
+++ src/debug.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars, no-useless-escape */
+
 // I tried adding an extra \ next to the d and w, I'm sure what's still wrong 
 const isValidCompanyUsername = (username, firstName, lastName) => {
   const regex = new RegExp(`^(sales|tech)\\d\\w-${firstName[0]}${lastName}\\d?$`);
